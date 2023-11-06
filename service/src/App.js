fd134ef4bc0eb79a3455f514a69a49ba0118bb41
jlangbid13 <qjlmlangbid@tip.edu.ph>
2023-11-07 00:26:53 +0800

    mayloginnanigga

---
+++ service/src/App.js
@@ -0,0 +1,26 @@
+
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nigga from './nigga'; // Assuming your component is in a file named "nigga.js"
+import Sched from './sections/Sched';
+import Homepage from './components/homepage/homepage';
+import Login from './components/login/login';
+import Register from './components/register/register';
+
+function App() {
+  const [user, setLoginUser] = useState({});
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nigga />} />
+        <Route path="/" element={user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />} />
+          <Route path="/login" element={<Login setLoginUser={setLoginUser} />} />
+          <Route path="/register" element={<Register />} />
+        <Route path="/Sched" element={<Sched />} />
+        {/* Define more routes for other components as needed */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
